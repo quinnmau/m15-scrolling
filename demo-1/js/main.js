d44d1e42bc682f7851d2b002f5fb0c69fbbd10f1
quinnmau <mauq@uw.edu>
2016-06-04 16:58:24 -0700

    map is on

--- demo-1/js/main.js
+++ demo-1/js/main.js
@@ -22,13 +22,30 @@ $(function() {
                 .y('population')
                 .yLabel('CO2 Emissions (kt)')
                 .xLabel('Year');
+                
+  var united = MapPlot()
+                .lat('lat')
+                .lon('lon')
+                .iden('name');
   
   
   // Instantiate your chart with given settings
   var chartWrapper = d3.select('#vis');
   
+  function showVis() {
+    d3.select('#vis')
+      .transition()
+      .duration(1500)
+      .style('opacity', 1.0);
+  }
+    
+  function hideVis() {
+    d3.select('#vis')
+      .transition()
+      .duration(0)
+      .style('opacity', 0);
+  }
   
-
   var updateBar = function(index, val, data) {
       switch(index) {
         case 11:
@@ -73,20 +90,6 @@ $(function() {
       chartWrapper.datum([data]).call(graph);
     };
     
-    function showVis() {
-      d3.select('#vis')
-        .transition()
-        .duration(1500)
-        .style('opacity', 1.0);
-    }
-    
-    function hideVis() {
-      d3.select('#vis')
-        .transition()
-        .duration(0)
-        .style('opacity', 0);
-    }
-    
     //determine which data to grab
     var getData = function(index) {
         if (index <= 9) {
@@ -95,6 +98,8 @@ $(function() {
           rate(index);
         } else if (index <= 18) {
           pop(index);
+        } else {
+          birds(index);
         }
     };
     
@@ -124,34 +129,12 @@ $(function() {
       });
     };
     
-    function objFilter1(obj) {
-      for (var i in obj) {
-        if (i !== 'us' && i !== 'year') 
-          delete obj[i];
-      }
-      return obj;
-    }
-    
-    function objFilter2(obj) {
-      for (var i in obj) {
-        console.log(i);
-        if (i !== 'us' && i !== 'year' && i !== 'china') 
-          delete obj[i];
-      }
-    }
-    
-    function objFilter3(obj) {
-      for (var i in obj) {
-        console.log(i);
-        if (i !== 'us' && i !== 'year' && i !== 'china' && i !== 'india' && i !== 'uk' && i !== 'japan') 
-          delete obj[i];
-      }
-    }
-    
     var pop = function(index) {
       d3.csv('data/population.csv', function(data) {
         switch(index) {
           case 15:
+            $('#vis').empty();
+            console.log('wet');
             showVis();
             var yVal = function(d) {
               return [{uk: +d.uk}];
@@ -176,6 +159,7 @@ $(function() {
             break;
           default:
             hideVis();
+            $('#vis').empty();
             var yVal = function(d) {
               return [
                 {uk: +d.uk}
@@ -192,26 +176,18 @@ $(function() {
         switch(index) {
           case 11:
             showVis();
-            var yVal = function(d) {
-              return [{india: +d.india}];
-            };
-            updateBar(index, yVal, data);
+            data.filter(function(d) {return objFilter1(d)});
+            chartWrapper.datum(data).call(graph2);
             break;
           case 12:
             showVis();
-            var yVal = function(d) {
-              return [
-                {india: +d.india},{china: +d.china}];
-            };
-            updateBar(index, yVal, data);
+            data.filter(function(d) {return objFilter2(d)});
+            chartWrapper.datum(data).call(graph2);
             break;
           case 13:
             showVis();
-            var yVal = function(d) {
-              return [
-                {india: +d.india},{china: +d.china},{japan: +d.japan},{us: +d.us},{uk: +d.uk}];
-            };
-            updateBar(index, yVal, data);
+            data.filter(function(d) {return objFilter3(d)});
+            chartWrapper.datum(data).call(graph2);
             break;
           default:
             hideVis();
@@ -219,7 +195,54 @@ $(function() {
             break;
         }
       });
+    };
+    
+    var birds = function(index) {
+      d3_queue.queue()
+        .defer(d3.json, 'json/us.json')
+        .defer(d3.csv, 'data/winter66avg.csv')
+        .defer(d3.csv, 'data/winter05avg.csv')
+        .await(function(error, map, pos1, pos2) {
+          switch(index) {
+            case 19:
+              $('#vis').empty();
+              showVis();
+              united.map(map);
+              chartWrapper.datum([]).call(united);
+              break;
+            case 20:
+              united.map(map);
+              showVis();
+              chartWrapper.datum(pos1).call(united);
+              break;
+          }
+        })
+    };
+    
+    function objFilter1(obj) {
+      for (var i in obj) {
+        if (i !== 'us' && i !== 'year') 
+          delete obj[i];
+      }
+      return obj;
     }
+    
+    function objFilter2(obj) {
+      for (var i in obj) {
+        console.log(i);
+        if (i !== 'us' && i !== 'year' && i !== 'china') 
+          delete obj[i];
+      }
+    }
+    
+    function objFilter3(obj) {
+      for (var i in obj) {
+        console.log(i);
+        if (i !== 'us' && i !== 'year' && i !== 'china' && i !== 'india' && i !== 'uk' && i !== 'japan') 
+          delete obj[i];
+      }
+    }
+    
   // Define a new scroller, and use the `.container` method to specify the desired container
   var scroll = scroller()
       .container(d3.select('#graphic'));
